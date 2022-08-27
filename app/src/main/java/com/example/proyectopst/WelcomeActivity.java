0fef7b2772ea3dd9aa0442672241791690166cbf
Dana Soriano <danpsori@espol.edu.ec>
2022-08-27 11:16:16 -0500

    Registro
    Abrir nuevo activity de principal a registro

--- app/src/main/java/com/example/proyectopst/WelcomeActivity.java
+++ app/src/main/java/com/example/proyectopst/WelcomeActivity.java
@@ -2,7 +2,9 @@ package com.example.proyectopst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -16,6 +18,10 @@ public class WelcomeActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
 
+
+    }
+    public void register(View view){
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity.class));
 
     }
 }
